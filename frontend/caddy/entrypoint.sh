7cbd6f477b5af78e7780798551bd96ec64ede470
sgjo <zzz8052@naver.com>
2026-02-03 15:59:06 +0900

    ed

--- frontend/caddy/entrypoint.sh
+++ frontend/caddy/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+# Next standalone 서버를 0.0.0.0로 바인딩 강제
+export HOSTNAME=0.0.0.0
+export PORT=3000
+
 node /srv/server.js &
 
 exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
